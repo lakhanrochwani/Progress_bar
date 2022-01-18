ab07914282bdd69bc0942c5f8d0168ba2930ef36
lakhanrochwani <rochwanilakhan@gmail.com>
2022-01-17 21:53:21 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const bar = document.querySelector('.progress-bar');
+console.log(bar);
+
+function loadBar() {
+  bar.classList.add('load');
+}
+
+// function reset() {
+//   bar.classList.add('reset');
+// }
+// window.addEventListener('load', (event) => {
+//   // insert code here
+//   bar.classList.add('load');
+// });
